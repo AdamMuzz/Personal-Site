333adb6bdc21b1e8e3d0448e81d94a62a0ee8021
Adam Muzzarelli <admuzzarelli@gmail.com>
2022-01-20 15:28:50 -0800

    added curvy section break

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <p>Test</p>
-      <p>Test 2</p>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div id='home'>
+				<p>Home</p>
+			</div>
+
+			<div className='spacer layer1' />
+
+			<div id='about'>
+				<p>About</p>
+			</div>
+
+			<div id='projects'>
+				<p>Projects</p>
+			</div>
+
+			<div id='activities'>
+				<p>Activites</p>
+			</div>
+
+			<div id='contact'>
+				<p>Contact</p>
+			</div>
+		</div>
+	);
 }
+
+/*
+	idea is to have transparent nav bar @ top
+		make it a sticky element in App
+	each 'section' is its own component
+	need a way to link nav bar to put sections in view
+
+	pages:
+		home
+		about
+		projects
+		activites
+		contact			
+ */
 
 export default App;
