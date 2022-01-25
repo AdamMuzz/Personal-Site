e1c45b44318d92d3a4a089a9d6b59b7376803429
Adam Muzzarelli <admuzzarelli@gmail.com>
2022-01-24 23:46:46 -0800

    made nav bar pretty

--- src/Header.js
+++ src/Header.js
@@ -8,13 +8,31 @@ import './Header.css';
 function NavBar() {
 	return (
 		<div id='NavBar'>
-			<button>HOME</button>
-			<button>ABOUT</button>
-			<button>PROJECTS</button>
-			<button>ACTIVITES</button>
-			<button>CONTACT</button>
+			<NavBtn name='HOME' />
+			<NavBtn name='ABOUT' />
+			<NavBtn name='PROJECTS' />
+			<NavBtn name='ACTIVITIES' />
+			<NavBtn name='CONTACT' />
 		</div>
 	);
 }
+
+function NavBtn(props) {
+	return (
+		<div className='Btn'>
+			<p>{props.name}</p>
+			<hr className='underline' />
+		</div>
+	);
+}
+
+/*function NavBtn(props) {
+	return (
+		<div className='NavBtn'>
+			<button>{props.name}</button>
+			<hr className='underline' />
+		</div>
+	);
+}*/
 
 export default NavBar;
