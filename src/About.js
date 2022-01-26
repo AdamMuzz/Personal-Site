528fa31ea4caa791c76cc55e49f29187eb108d12
Adam Muzzarelli <admuzzarelli@gmail.com>
2022-01-26 14:00:55 -0800

    added bio to About Me

--- src/About.js
+++ src/About.js
@@ -3,7 +3,28 @@ import './styles/About.css';
 function AboutSection() {
 	return (
 		<div id='About-BG'>
+
 			<h1>About Me</h1>
+			
+			<div>
+				{/* image of myself & bio */}
+				<p>
+					I'm currently a 2nd year Computer Science major at UCLA, with plans to double major in Business Economics.
+					Within the realm of CS, I'm very interested in UI/UX design and AI fields.
+					Aside from coding, business is my passion.
+					My good friend and I co-founded a startup called Bartr which aims to create a localized trading platform for college students to promote and facilitate meeting new people and other neighborly activities.
+					Building a business from the ground up is an exciting and rewarding experience that I hope to incorporate into my future career, as it allows me to use my love for coding, business, and design in a productive and meaningful way.
+					Outside of academics, I'm an avid home chef, love to workout, and am a passionate retail investor.
+					I especially love cooking breakfast, and consider myself in the top 1% of omelet makers worldwide.
+				</p>
+			</div>
+
+			<div>
+				{/* profile on left, skills on right */}
+			</div>
+
+			{/* download resume btn */}
+
 		</div>
 	);
 }
