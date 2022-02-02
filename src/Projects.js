832d5d949add20b4a4f197c502ba51c5399fc70a
Adam Muzzarelli <admuzzarelli@gmail.com>
2022-02-01 20:28:10 -0800

    added project cards

--- src/Projects.js
+++ src/Projects.js
@@ -1,9 +1,28 @@
 import './styles/Projects.css';
 
-function ProjectsSection () {
+function ProjectsSection() {
 	return (
 		<div id='Projects-BG'>
 			<h1 className='section-title'>My Projects</h1>
+
+			<div id='projects-container'>
+				<Project/>
+				<Project/>
+				<Project/>
+				<Project/>
+			</div>
+		</div>
+	);
+}
+
+function Project() {
+	return (
+		<div className='project-card'>
+			<div className='card-hover'>
+				<p id='card-name'>Name</p>
+				<p id='card-description'>short description</p>
+				<h1 id='card-plus'>+</h1>
+			</div>
 		</div>
 	);
 }
