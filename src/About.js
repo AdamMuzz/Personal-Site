a7d3066d925054dc431ce1f323db14595e49e600
Adam Muzzarelli <admuzzarelli@gmail.com>
2023-07-08 21:41:49 -0700

    update self's info/content

--- src/About.js
+++ src/About.js
@@ -11,8 +11,8 @@ function AboutSection() {
 				<img id='headshot' src={headshot} alt='HeadShot'/>
 				<div>
 					<p className='section-text'>
-						&emsp;I'm currently a 3rd year student at UCLA, double majoring in computer science and economics.
-						Within the realm of CS, I'm very interested in UI/UX design and AI fields.
+						&emsp;I'm currently a 4th year student at UCLA pursuing a BS in computer science and BA in economics.
+						Within the realm of CS, I'm very interested in ML/AI, economic forecasting, and UI/UX design.
 					</p>
 					<p className='section-text'>
 						&emsp;Aside from coding, business is my passion.
@@ -31,7 +31,7 @@ function AboutSection() {
 					<h2 className='subheader'>Profile</h2>
 
 					<p className='section-text'>
-						I'm currently seeking a Summer internship for computer science in Los Angeles or Bay Area (US).
+						I'm currently seeking a summer internship in Los Angeles or the Bay Area (US).
 						If you think I'm a good fit, please contact me via <a href='mailto: admuzzarelli@gmail.com'>admuzzarelli@gmail.com</a>.
 					</p>
 
@@ -48,17 +48,20 @@ function AboutSection() {
 				<div className='ps-wrapper'>
 					<h2 className='subheader'>Skills</h2>
 
+					<p className='subheader2'>Python</p>
+					<SkillBar size={9}/>
+
 					<p className='subheader2'>React/Javascript</p>
 					<SkillBar size={9}/>
 
-					<p className='subheader2'>C++/C</p>
-					<SkillBar size={7}/>
-
-					<p className='subheader2'>Python</p>
+					<p className='subheader2'>C++</p>
 					<SkillBar size={8}/>
 
 					<p className='subheader2'>HTML/CSS</p>
 					<SkillBar size={8}/>
+
+					<p className='subheader2'>R</p>
+					<SkillBar size={7}/>
 				</div>
 			</div>
 
